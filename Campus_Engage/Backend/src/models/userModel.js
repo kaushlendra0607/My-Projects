284251995a1016a9a400e10c9061e6f1aab57a19
kaushlendra-2005 <kaushlendrapratapsingh631@gmail.com>
2025-12-08 17:15:16 +0530

    Created user schema

---
+++ Campus_Engage/Backend/src/models/userModel.js
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    userName: {
+        type: String,
+        required: true,
+        trim: true, index: true, unique: true, lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    avatar: {
+        type: String,
+        required:true
+    },
+    password: {
+        type: String,
+        required: [true,"Password is required!"]
+    },
+    refreshToken: { type: String }
+
+},{timestamps:true});
+
+// 1. Encrypt Password before saving
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+// 3. Methods to generate Tokens
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    );
+};
+
+const userModel = mongoose.models.User || mongoose.model('User',userSchema);
+
+export default userModel;
