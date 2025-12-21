5684d84dca7135a7dc56f6897eeb660dbe994335
kaushlendra-2005 <kaushlendrapratapsingh631@gmail.com>
2025-12-21 19:14:17 +0530

    Login Page completed.

--- Campus_Engage/Frontend/src/pages/LoginPage.jsx
+++ Campus_Engage/Frontend/src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import api from '../config/api.js';
 import { toast } from "react-toastify";
 import { Helmet } from "react-helmet";
+import './login.css';
 
 const LoginPage = ({ setToken }) => {
   // Toggle State: "Login" or "Sign Up"
@@ -68,7 +69,7 @@ const LoginPage = ({ setToken }) => {
   }
 
   return (
-    <section className='login-container'>
+    <section className='login'>
       <Helmet>
         <title>{currentState === "Login" ? "Login" : "Sign Up"}</title>
         <meta
@@ -78,56 +79,56 @@ const LoginPage = ({ setToken }) => {
             : "Join the best Event Management Platform. Create an account to start participating in college events."}
         />
       </Helmet>
-      <form onSubmit={onSubmitHandler} className='flex flex-col gap-4 w-[300px] m-auto mt-10'>
+      <form onSubmit={onSubmitHandler} className='login-form'>
 
-        <div className='flex justify-between items-center mb-4'>
-          <h2 className='text-2xl font-bold'>{currentState}</h2>
-        </div>
+        <h2 className='login-head'>{currentState}</h2>
 
         {/* --- Fields for Sign Up Only --- */}
         {currentState === "Sign Up" && (
           <>
             <input type="text" placeholder='Full Name' required
               value={fullName} onChange={(e) => setFullName(e.target.value)}
-              className='border p-2 rounded' />
+              className='login-input' />
 
             <input type="text" placeholder='User Name' required
               value={userName} onChange={(e) => setUserName(e.target.value)}
-              className='border p-2 rounded' />
+              className='login-input' />
 
             <input type="number" placeholder='Batch Year' required
               value={batch} onChange={(e) => setBatch(e.target.value)}
-              className='border p-2 rounded' />
+              className='login-input' />
 
-            <label className="text-sm text-gray-600">Profile Picture</label>
+            <label className="profile-label">Profile Picture</label>
             {/* 🛑 FIX: No 'value' attribute here! */}
             <input type="file" required
               onChange={(e) => setAvatar(e.target.files[0])}
-              className='border p-2 rounded' />
+              className='login-input' />
           </>
         )}
 
         {/* --- Common Fields --- */}
         <input type="email" placeholder='Email' required
           value={email} onChange={(e) => setEmail(e.target.value)}
-          className='border p-2 rounded' autoComplete='username' />
+          className='login-input' autoComplete='username' />
 
         <input type="password" placeholder='Password' required
           value={password} onChange={(e) => setPassword(e.target.value)}
-          className='border p-2 rounded' autoComplete={ currentState === "Login" ? "current-password" : "new-password" } />
+          className='login-input' autoComplete={ currentState === "Login" ? "current-password" : "new-password" } />
 
-        <button type='submit' disabled={isLoading} className='bg-black text-white p-2 rounded'>
+        <button type='submit' disabled={isLoading} className='submit'>
           {isLoading ? "Processing..." : (currentState === "Sign Up" ? "Create Account" : "Login")}
         </button>
 
         {/* --- Toggle Switch --- */}
-        <p className='text-sm mt-2 cursor-pointer text-blue-600' onClick={() => setCurrentState(currentState === "Login" ? "Sign Up" : "Login")}>
+        <p className='options' onClick={() => setCurrentState(currentState === "Login" ? "Sign Up" : "Login")}>
           {currentState === "Login"
             ? "New here? Create an account"
             : "Already have an account? Login here"}
         </p>
 
       </form>
+      <img src="https://i.pinimg.com/736x/f2/32/6f/f2326fb1bb41b112b7bca9f358eebae1.jpg"
+       alt="Event Image" className='event-img' />
     </section>
   )
 }
--- Campus_Engage/Frontend/vite.config.js
+++ Campus_Engage/Frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     strictPort: true // Optional: Crash if 3000 is busy (instead of trying 3001)
