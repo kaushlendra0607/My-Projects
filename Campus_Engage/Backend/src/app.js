32e3199150d169b24886021698b587b8eba9a518
kaushlendra-2005 <kaushlendrapratapsingh631@gmail.com>
2025-12-30 21:42:00 +0530

    Now deploying backend to render.

--- Campus_Engage/Backend/src/app.js
+++ Campus_Engage/Backend/src/app.js
@@ -10,7 +10,7 @@ import paymentRouter from "./routes/paymentRouter.js";
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || 'https://campusengage.netlify.app/',
     credentials: true
 }));
 
